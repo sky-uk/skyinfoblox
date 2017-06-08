8524425e3fadf0cfccf743a9d513902aae93f50e
Craig Duffin <craig.duffin@sky.uk>
2017-06-08 16:42:35 +0100

    Go lint changes

--- cli/functions.go
+++ cli/functions.go
@@ -4,6 +4,7 @@ import (
 	"github.com/crackcomm/go-clitable"
 )
 
+// PrettyPrintMany - Pretty prints maps as tables
 func PrettyPrintMany(headers []string, rows []map[string]interface{}) {
 	table := clitable.New(headers)
 	for _, row := range rows {
@@ -12,6 +13,7 @@ func PrettyPrintMany(headers []string, rows []map[string]interface{}) {
 	table.Print()
 }
 
+// PrettyPrintSingle - Pretty prints map as tables
 func PrettyPrintSingle(row map[string]interface{}) {
 	clitable.PrintHorizontal(row)
 }
