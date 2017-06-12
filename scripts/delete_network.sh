baea5bf11e7e6ed89ecf9558f7b54d7608ee5071
Marco Masetti <marco.masetti@sky.uk>
2017-06-12 23:55:09 +0100

    deleted 2 files

--- scripts/delete_network.sh
+++
@@ -1,6 +0,0 @@
-if [ $# -lt 1 ]
-then
-    echo "Usage: delete_network.sh <an network object reference>\n"
-    exit
-fi
-curl -w "\nStatus Code: %{http_code}\n" -k1 -u admin:infoblox -X DELETE https://h1infoblox.devops.int.ovp.bskyb.com/wapi/v2.3.1/$1
