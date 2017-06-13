3dffd11f6b6a35b91d200e6b772b28b3049cd985
rle27 <rui.leitemayermoreira@sky.uk>
2017-06-13 15:52:51 +0100

    fixed fmt issues

--- api/records/create_record_test.go
+++ api/records/create_record_test.go
@@ -16,7 +16,6 @@ func createGenericRecordSetup() *CreateRecordAPI {
 	return NewCreateRecord("a", newARecord)
 }
 
-
 func createARecordSetup() *CreateRecordAPI {
 	newARecord := ARecord{
 		IPv4:    "1.1.1.1",
@@ -37,8 +36,7 @@ func TestNewCreateARecord(t *testing.T) {
 	assert.Equal(t, http.MethodPost, createARecordAPI.Method())
 }
 
-
-func TestCreateARecordEndpoint(t *testing.T){
+func TestCreateARecordEndpoint(t *testing.T) {
 	createARecordAPI := createARecordSetup()
 	assert.Equal(t, "/wapi/v2.3.1/record:a", createARecordAPI.Endpoint())
 
