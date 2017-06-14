df851116dd9af20944e4e7c4fecb501e14e69adb
rle27 <rui.leitemayermoreira@sky.uk>
2017-06-14 11:51:35 +0100

    changed the create function a bit to try and solve the error I am getting on the provider side

--- api/records/create_record.go
+++ api/records/create_record.go
@@ -10,6 +10,11 @@ type CreateRecordAPI struct {
 	*api.BaseAPI
 }
 
+// CreateRecordAPI base object.
+type CreateARecordAPI struct {
+	*api.BaseAPI
+}
+
 // NewCreateRecord returns a new object of CreateRecordAPI.
 func NewCreateRecord(recordType string, requestPayload GenericRecord) *CreateRecordAPI {
 	this := new(CreateRecordAPI)
@@ -20,8 +25,13 @@ func NewCreateRecord(recordType string, requestPayload GenericRecord) *CreateRec
 // NewCreateARecord - Creates a new A record
 func NewCreateARecord(requestPayload ARecord) *CreateRecordAPI {
 	this := new(CreateRecordAPI)
-	this.BaseAPI = api.NewBaseAPI(http.MethodPost, "/wapi/v2.3.1/record:a", requestPayload, nil)
+	this.BaseAPI = api.NewBaseAPI(http.MethodPost, "/wapi/v2.3.1/record:a", requestPayload, new(ARecord))
 	return this
+}
+
+// GetResponse returns ResponseObject of CreateRecordAPI.
+func (c CreateARecordAPI) GetResponse() ARecord {
+	return c.ResponseObject().(ARecord)
 }
 
 // GetResponse returns ResponseObject of CreateRecordAPI.
