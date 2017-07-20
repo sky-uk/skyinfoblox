468b6622c25d2ce43da4e7c5453b3e968448e2b3
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-20 14:08:33 +0100

    update method implemented and tests

---
+++ api/dhcp_range/update_range_test.go
@@ -0,0 +1,49 @@
+package dhcprange
+
+import (
+//	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func UpdateDHCPRangeSetup() *UpdateDHCPRangeAPI {
+	dhcpRange := DHCPRange{
+		Network:     "192.168.0.0/24",
+		NetworkView: "default",
+		Start:       "192.168.0.50",
+		End:         "192.168.0.60",
+		Restart:     true,
+	}
+	dhcpMember := Member{ElementType: "dhcpmember", Name: "test.example.com", IPv4Address: "192.168.0.10"}
+	dhcpRange.Ref = "ZG5zLm5ldHdvcmskMTAuMTAuMTAuMC8yNC8w:192.168.0.0/24/default"
+	dhcpRange.Member = dhcpMember
+	dhcpRange.ServerAssociation = "MEMBER"
+	newUpdateRangeAPI := NewUpdateDHCPRange(dhcpRange)
+	responseString := "ZG5zLm5ldHdvcmskMTAuMTAuMTAuMC8yNC8w:192.168.0.0/24/default"
+	newUpdateRangeAPI.SetResponseObject(&responseString)
+	return newUpdateRangeAPI
+}
+
+
+
+func TestMethodDHCPRange(t *testing.T) {
+	testRelay := UpdateDHCPRangeSetup()
+	assert.Equal(t, http.MethodPut, testRelay.Method())
+
+}
+
+
+func TestEndpointDHCPRange(t *testing.T){
+	testRelay := UpdateDHCPRangeSetup()
+	assert.Equal(t, "/wapi/v2.3.1/range/ZG5zLm5ldHdvcmskMTAuMTAuMTAuMC8yNC8w:192.168.0.0/24/default", testRelay.Endpoint())
+
+}
+
+
+func TestNewUpdateDHCPRangeResponse(t *testing.T) {
+	testRelay := UpdateDHCPRangeSetup()
+	assert.Equal(t, "ZG5zLm5ldHdvcmskMTAuMTAuMTAuMC8yNC8w:192.168.0.0/24/default", testRelay.GetResponse())
+
+
+}
