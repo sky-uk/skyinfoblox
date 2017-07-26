b9418390d1520aa57b1b8ea9fda054281b1cf432
Marco Masetti <marco.masetti@sky.uk>
2017-07-26 12:16:56 +0100

    completed ExternalServer attributes list

--- api/zoneauth/zone_auth_model.go
+++ api/zoneauth/zone_auth_model.go
@@ -212,8 +212,14 @@ type DNSSecKey struct {
 
 // ExternalServer : external DNS server
 type ExternalServer struct {
-	Address string `json:"address,omitempty"`
-	Name    string `json:"name,omitempty"`
+	Address                      string `json:"address"`
+	Name                         string `json:"name"`
+	SharedWithMSParentDelegation bool   `json:"shared_with_ms_parent_delegation,omitempty"` //cannot be updated nor written
+	Stealth                      bool   `json:"stealth,omitempty"`                          //defaults to false
+	TsigKey                      string `json:"tsig_key,omitempty"`                         //defaults to empty
+	TsigKeyAlg                   string `json:"tsig_key_alg,omitempty"`                     // defaults to HMAC-MD5
+	TsigKeyName                  string `json:"tsig_key_name,omitempty"`                    //defaults to empty
+	UseTsigKeyName               bool   `json:"use_tsig_key_name,omitempty"`                //defaults to false
 }
 
 // MemberServer : Grid member struct
