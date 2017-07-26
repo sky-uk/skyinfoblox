6740323e9966c00c5d962046cf5725469aac69a4
Marco Masetti <marco.masetti@sky.uk>
2017-07-26 16:19:20 +0100

    Set bool attributes to be pointers

--- api/zoneauth/zone_auth_model.go
+++ api/zoneauth/zone_auth_model.go
@@ -214,12 +214,12 @@ type DNSSecKey struct {
 type ExternalServer struct {
 	Address                      string `json:"address"`
 	Name                         string `json:"name"`
-	SharedWithMSParentDelegation bool   `json:"shared_with_ms_parent_delegation,omitempty"` //cannot be updated nor written
-	Stealth                      bool   `json:"stealth,omitempty"`                          //defaults to false
+	SharedWithMSParentDelegation *bool  `json:"shared_with_ms_parent_delegation,omitempty"` //cannot be updated nor written
+	Stealth                      *bool  `json:"stealth,omitempty"`                          //defaults to false
 	TsigKey                      string `json:"tsig_key,omitempty"`                         //defaults to empty
 	TsigKeyAlg                   string `json:"tsig_key_alg,omitempty"`                     // defaults to HMAC-MD5
 	TsigKeyName                  string `json:"tsig_key_name,omitempty"`                    //defaults to empty
-	UseTsigKeyName               bool   `json:"use_tsig_key_name,omitempty"`                //defaults to false
+	UseTsigKeyName               *bool  `json:"use_tsig_key_name,omitempty"`                //defaults to false
 }
 
 // MemberServer : Grid member struct
