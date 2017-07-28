bb864d4f9dfb30a18eca982b984d7d30ceb21885
Paul Warwick <paul.warwick@sky.uk>
2017-07-28 18:12:54 +0100

    Added Admin Group package to API

---
+++ api/admingroup/admingroup_model.go
@@ -0,0 +1,24 @@
+package admingroup
+
+const adminGroupEndpoint = "/wapi/v2.3.1"
+
+// IBXAdminGroup : Admin group definition
+type IBXAdminGroup struct {
+	Reference      string   `json:"_ref,omitempty"`
+	AccessMethod   string   `json:"access_method,omitempty"`
+	Comment        string   `json:"comment,omitempty"`
+	Disable        *bool    `json:"disable,omitempty"`
+	EmailAddresses []string `json:"email_addresses,omitempty"`
+	Name           string   `json:"name,omitempty"`
+	Roles          []string `json:"roles,omitempty"`
+	SuperUser      *bool    `json:"superuser,omitempty"`
+}
+
+// IBXAdminGroupReferences : A list of admin group references
+type IBXAdminGroupReferences []IBXAdminGroupReference
+
+// IBXAdminGroupReference : A reference object for an admin group
+type IBXAdminGroupReference struct {
+	Reference      string `json:"_ref"`
+	AdminGroupName string `json:"name"`
+}
