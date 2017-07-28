88c0f16da09369631692437f178db45239dfebf9
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-28 16:37:58 +0100

    updated for a more simple format

--- api/adminuser/adminuser.go
+++ api/adminuser/adminuser.go
@@ -7,50 +7,39 @@ import (
 	"strings"
 )
 
-//BaseAdminUserAPI - Base struce
-type BaseAdminUserAPI struct {
-	*api.BaseAPI
-}
 
 var endPoint string
 
 //NewCreateAdminUser - Create function
-func NewCreateAdminUser(newUser AdminUser) *BaseAdminUserAPI {
-	this := new(BaseAdminUserAPI)
+func NewCreateAdminUser(newUser AdminUser) *api.BaseAPI {
 	endPoint = "/wapi/v2.2.2/adminuser"
-	this.BaseAPI = api.NewBaseAPI(http.MethodPost, endPoint, newUser, new(string))
-	return this
+	createUserAPI := api.NewBaseAPI(http.MethodPost, endPoint, newUser, new(string))
+	return createUserAPI
 }
 
 //NewGetAdminUser - Get a User
-func NewGetAdminUser(ref string, returnFields []string) *BaseAdminUserAPI {
-	this := new(BaseAdminUserAPI)
+func NewGetAdminUser(ref string, returnFields []string) *api.BaseAPI {
 	if returnFields != nil && len(returnFields) > 0 {
 		endPoint = fmt.Sprintf("/wapi/v2.2.2/%s/?_return_fields=%s", ref, strings.Join(returnFields, ","))
 	} else {
 		endPoint = fmt.Sprintf("/wapi/v2.2.2/%s", ref)
 	}
-	this.BaseAPI = api.NewBaseAPI(http.MethodGet, endPoint, nil, new(AdminUser))
-	return this
+	updateUserAPI := api.NewBaseAPI(http.MethodGet, endPoint, nil, new(AdminUser))
+	return updateUserAPI
 }
 
 //NewDeleteAdminUser - Deletes the user
-func NewDeleteAdminUser(ref string) *BaseAdminUserAPI {
-	this := new(BaseAdminUserAPI)
+func NewDeleteAdminUser(ref string) *api.BaseAPI {
 	endPoint = fmt.Sprintf("/wapi/v2.2.2/%s", ref)
-	this.BaseAPI = api.NewBaseAPI(http.MethodDelete, endPoint, nil, new(AdminUser))
-	return this
+	deleteUserAPI  := api.NewBaseAPI(http.MethodDelete, endPoint, nil, new(AdminUser))
+	return deleteUserAPI
 }
 
 // NewUpdateAdminUser - Updates the user
-func NewUpdateAdminUser(updateUser AdminUser) *BaseAdminUserAPI {
-	this := new(BaseAdminUserAPI)
+func NewUpdateAdminUser(updateUser AdminUser) *api.BaseAPI {
 	endPoint = fmt.Sprintf("/wapi/v2.2.2/%s", updateUser.Ref)
-	this.BaseAPI = api.NewBaseAPI(http.MethodPut, endPoint, nil, new(AdminUser))
-	return this
+	updateUserAPI := api.NewBaseAPI(http.MethodPut, endPoint, nil, new(AdminUser))
+	return updateUserAPI
 }
 
-// GetResponse casts the response object to string
-func (gu BaseAdminUserAPI) GetResponse() string {
-	return *gu.ResponseObject().(*string)
-}
+
