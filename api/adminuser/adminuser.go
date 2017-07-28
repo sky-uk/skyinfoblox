c83c6b9ca3152d1534cda000d39f793060a72adc
rle27 <rui.leitemayermoreira@sky.uk>
2017-07-28 16:21:34 +0100

    renamed base struct

--- api/adminuser/adminuser.go
+++ api/adminuser/adminuser.go
@@ -7,24 +7,24 @@ import (
 	"strings"
 )
 
-//CreateAdminuserAPI
-type AdminUserAPI struct {
+//AdminUserAPI - Base struce
+type BaseAdminUserAPI struct {
 	*api.BaseAPI
 }
 
 var endPoint string
 
 //NewCreateAdminUser - Create function
-func NewCreateAdminUser(newUser AdminUser) *AdminUserAPI {
-	this := new(AdminUserAPI)
+func NewCreateAdminUser(newUser AdminUser) *BaseAdminUserAPI {
+	this := new(BaseAdminUserAPI)
 	endPoint = "/wapi/v2.2.2/adminuser"
 	this.BaseAPI = api.NewBaseAPI(http.MethodPost, endPoint, newUser, new(string))
 	return this
 }
 
 //NewGetAdminUser - Get a User
-func NewGetAdminUser(ref string, returnFields []string) *AdminUserAPI {
-	this := new(AdminUserAPI)
+func NewGetAdminUser(ref string, returnFields []string) *BaseAdminUserAPI {
+	this := new(BaseAdminUserAPI)
 	if returnFields != nil && len(returnFields) > 0 {
 		endPoint = fmt.Sprintf("/wapi/v2.2.2/%s/?_return_fields=%s", ref, strings.Join(returnFields, ","))
 	} else {
@@ -34,21 +34,21 @@ func NewGetAdminUser(ref string, returnFields []string) *AdminUserAPI {
 	return this
 }
 
-func NewDeleteAdminUser(ref string) *AdminUserAPI {
-	this := new(AdminUserAPI)
+func NewDeleteAdminUser(ref string) *BaseAdminUserAPI {
+	this := new(BaseAdminUserAPI)
 	endPoint = fmt.Sprintf("/wapi/v2.2.2/%s", ref)
 	this.BaseAPI = api.NewBaseAPI(http.MethodDelete, endPoint, nil, new(AdminUser))
 	return this
 }
 
-func NewUpdateAdminUser(updateUser AdminUser) *AdminUserAPI {
-	this := new(AdminUserAPI)
+func NewUpdateAdminUser(updateUser AdminUser) *BaseAdminUserAPI {
+	this := new(BaseAdminUserAPI)
 	endPoint = fmt.Sprintf("/wapi/v2.2.2/%s", updateUser.Ref)
 	this.BaseAPI = api.NewBaseAPI(http.MethodPut, endPoint, nil, new(AdminUser))
 	return this
 }
 
 // GetResponse casts the response object to string
-func (gu AdminUserAPI) GetResponse() string {
+func (gu BaseAdminUserAPI) GetResponse() string {
 	return *gu.ResponseObject().(*string)
 }
