4aec54d9eaf705b0eba1ae7a73fc377572393ce5
Paul Warwick <paul.warwick@sky.uk>
2017-08-01 10:24:36 +0100

    Improved error messages for admin group CLI

--- cli/admingroup_show_all.go
+++ cli/admingroup_show_all.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/sky-uk/skyinfoblox"
 	"github.com/sky-uk/skyinfoblox/api/admingroup"
+	"net/http"
 	"os"
 )
 
@@ -12,8 +13,9 @@ func showAllAdminGroup(client *skyinfoblox.InfobloxClient, flagSet *flag.FlagSet
 
 	showAllAdminGroup := admingroup.NewGetAll()
 	err := client.Do(showAllAdminGroup)
-	if err != nil {
-		fmt.Printf("\nError whilst retrieving a list of admin groups: %+v", err)
+	httpStatus := showAllAdminGroup.StatusCode()
+	if err != nil || httpStatus < http.StatusOK || httpStatus >= http.StatusBadRequest {
+		fmt.Printf("\nError whilst retrieving a list of admin groups. HTTP status: %d. Error: %+v\n", httpStatus, err)
 		os.Exit(1)
 	}
 	allAdminGroups := showAllAdminGroup.ResponseObject().(*[]admingroup.IBXAdminGroupReference)
