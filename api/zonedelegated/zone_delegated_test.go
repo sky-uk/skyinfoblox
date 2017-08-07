6778eef89cf03b791aadb9d7ca2b13497ff00f49
rle27 <rui.leitemayermoreira@sky.uk>
2017-08-07 15:07:09 +0100

    branch rename

--- api/zonedelegated/zone_delegated_test.go
+++ api/zonedelegated/zone_delegated_test.go
@@ -1,7 +1,7 @@
 package zonedelegated
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/sky-uk/skyinfoblox/api"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -9,7 +9,8 @@ import (
 	"github.com/sky-uk/skyinfoblox/api/common"
 )
 
-func setupZoneDelegated() *api.BaseAPI {
+func setupZoneDelegated(action string) *api.BaseAPI {
+
 	delegateObject := common.ExternalServer{
 		Address:"172.16.0.1",
 		Name: "dns1.testdomain.com",
@@ -18,11 +19,85 @@ func setupZoneDelegated() *api.BaseAPI {
 	newZone := ZoneDelegated{
 		Address: "10.0.0.1",
 		Comment: "This is a comment",
-		DelegateTo: delegateObject,
+		DelegateTo: []common.ExternalServer{delegateObject},
 		DelegatedTTL: 1234,
 		Disable: &disableZone,
 		DnsFqdn: "example.com",
+		View: "default",
+		ZoneFormat: "FORWARD",
+	}
+
+	switch action {
+
+	case "create":
+		zoneAPI := NewCreateZoneDelegated(newZone)
+		response := "/wapi/v2.6.1/zone_delegated/blablalba:example.com/default"
+		zoneAPI.SetResponseObject(&response)
+		return zoneAPI
+	case "get":
+		returnFields := []string{}
+		zoneAPI := NewGetZoneDelegated("blablalba:example.com",returnFields)
+		return  zoneAPI
+	case "getWithFields":
+		returnFields := []string{"delegate_to","fqdn"}
+		zoneAPI := NewGetZoneDelegated("blablalba:example.com",returnFields)
+		return  zoneAPI
+
+	case "delete":
+		zoneAPI := NewDeleteZoneDelegated("blablalba:example.com")
+		return zoneAPI
+	case "update":
+		zoneAPI := NewUpdateZoneDelegated("blablalba:example.com",newZone)
+		return  zoneAPI
+	default:
+		return nil
 	}
 
 
+}
+
+func TestCreateZoneDelegatedMethod(t *testing.T){
+	newZone := setupZoneDelegated("create")
+	assert.Equal(t, http.MethodPost, newZone.Method() )
+}
+
+func TestCreateZoneDelegatedEndpoint(t *testing.T){
+	newZone := setupZoneDelegated("create")
+	assert.Equal(t, "/wapi/v2.3.1/zone_delegated", newZone.Endpoint() )
+}
+
+func TestGetZoneDelegatedMethod(t *testing.T) {
+	newZone := setupZoneDelegated("get")
+	assert.Equal(t, http.MethodGet, newZone.Method() )
+}
+
+func TestGetZoneDelegatedEndpoint(t *testing.T) {
+	newZone := setupZoneDelegated("get")
+	assert.Equal(t, "/wapi/v2.3.1/blablalba:example.com", newZone.Endpoint())
+}
+
+func TestGetZoneDelegatedEndpointWithFields(t *testing.T) {
+	newZone := setupZoneDelegated("getWithFields")
+	assert.Equal(t, "/wapi/v2.3.1/blablalba:example.com/?_return_fields=delegate_to,fqdn", newZone.Endpoint())
+}
+
+
+func TestDeleteZoneDelegatedMethod(t *testing.T) {
+	newZone := setupZoneDelegated("delete")
+	assert.Equal(t, http.MethodDelete, newZone.Method())
+}
+
+func TestDeleteZoneDelegatedEndpoint(t *testing.T) {
+	newZone := setupZoneDelegated("delete")
+	assert.Equal(t, "/wapi/v2.3.1/blablalba:example.com", newZone.Endpoint())
+}
+
+func TestUpdateZoneDelegatedMethod(t *testing.T) {
+	newZone := setupZoneDelegated("update")
+	assert.Equal(t, http.MethodPut, newZone.Method())
+}
+
+func TestUpdateZoneDelegatedEndpoing(t *testing.T) {
+	newZone := setupZoneDelegated("update")
+	assert.Equal(t, "/wapi/v2.3.1/blablalba:example.com", newZone.Endpoint())
 }
