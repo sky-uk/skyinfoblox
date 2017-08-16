a43d39fcb191ae5446a1d369f4ee8b2cc5d0d791
rle27 <rui.leitemayermoreira@sky.uk>
2017-08-16 11:36:16 +0100

    zone stub initial code

---
+++ api/zonestub/zone_stub.go
@@ -0,0 +1,35 @@
+package zonestub
+
+import (
+	"fmt"
+	"github.com/sky-uk/skyinfoblox/api"
+	"net/http"
+	"strings"
+)
+
+var endPoint string
+
+// NewCreate - Create a new sbut zone
+func NewCreate(newzoneStub ZoneStub) *api.BaseAPI{
+
+}
+
+//NewGet - Get a single stub zone
+func NewGet(ref string) *api.BaseAPI{
+
+}
+// NewGetAll - Get all stub zones
+func NewGetAll() *api.BaseAPI {
+
+}
+
+// NewUpdate - Updates an existing Zone
+func NewUpdate(updateZoneStub ZoneStub) *api.BaseAPI {
+
+}
+
+// NewDelete - Deletes an existing zone
+
+func NewDelete(ref string) *api.BaseAPI {
+
+}
