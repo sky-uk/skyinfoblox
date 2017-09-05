5886824d1f09defba025f34ec526f436ee79da6c
Craig Duffin <craig.duffin@sky.uk>
2017-09-05 16:47:36 +0100

    Added api CRUD methods

--- api/dnsview/dnsview.go
+++ api/dnsview/dnsview.go
@@ -1,1 +1,40 @@
-package dns_view
+package dnsview
+
+import (
+	"github.com/sky-uk/skyinfoblox/api"
+	"net/http"
+	"strings"
+)
+
+
+//NewCreate : used to create a new DNSView object
+func NewCreate(dnsView DNSView) *api.BaseAPI{
+	createDnsViewAPI := api.NewBaseAPI(http.MethodPost, wapiVersion+dnsViewEndpoint, dnsView,new(string))
+	return createDnsViewAPI
+}
+
+//NewUpdate : used to update an existing DNSView object
+func NewUpdate(dnsView DNSView, returnFieldList []string) *api.BaseAPI{
+	reference := dnsView.Reference+"?_return_fields=" + strings.Join(returnFieldList, ",")
+	createDnsViewAPI := api.NewBaseAPI(http.MethodPost, wapiVersion+dnsViewEndpoint+reference, dnsView,new(DNSView))
+	return createDnsViewAPI
+}
+
+//NewGetAll : used to retrieve all DNSView objects
+func NewGetAll() *api.BaseAPI{
+	getAllDnsViewAPI := api.NewBaseAPI(http.MethodGet, wapiVersion+dnsViewEndpoint,nil,new([]DNSView))
+	return getAllDnsViewAPI
+}
+
+//NewGet : used to retrieve a DNSView object
+func NewGet(reference string, returnFieldList []string) *api.BaseAPI{
+	reference += "?_return_fields=" + strings.Join(returnFieldList, ",")
+	getDnsViewAPI := api.NewBaseAPI(http.MethodGet, wapiVersion+dnsViewEndpoint+reference,nil,new(DNSView))
+	return getDnsViewAPI
+}
+
+//NewDelete : used to delete a DNSView object
+func NewDelete(reference string) *api.BaseAPI{
+	deleteDnsViewAPI := api.NewBaseAPI(http.MethodDelete, wapiVersion+dnsViewEndpoint+reference,nil,new(string))
+	return deleteDnsViewAPI
+}
